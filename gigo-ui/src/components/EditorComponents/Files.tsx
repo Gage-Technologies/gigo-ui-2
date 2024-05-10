a8548a19e1e4bf3c710d6ea662422a2f6c490745
Daniel Ulrich <tmwap76@gmail.com>
2024-05-10 13:52:30 -0500

    Create Project and Public Config pages done

---
+++ gigo-ui/src/components/EditorComponents/Files.tsx
@@ -0,0 +1,328 @@
+import React, { useState } from 'react';
+import { RiArrowDownSLine, RiArrowRightSLine } from 'react-icons/ri';
+import FILE_ICONS from './FileIcons';
+import { Box, Button, ButtonBase, CircularProgress, createTheme, PaletteMode, useMediaQuery } from "@mui/material";
+import { getAllTokens } from "@/theme";
+import call from "../../services/api-call";
+import config from "../../config";
+import swal from "sweetalert";
+import { ThreeDots } from "react-loading-icons";
+import FolderIcon from '@mui/icons-material/Folder';
+import { initialAuthStateUpdate, updateAuthState } from "../../reducers/auth/auth";
+import { useNavigate } from "react-router-dom";
+
+interface Children {
+    children: React.ReactNode;
+}
+
+interface ChildrenName {
+    children: React.ReactNode;
+    name: string;
+    padding: string;
+    filepath: string;
+    repoId: string
+    handleFileSelect: any
+    selectedFile: object
+}
+
+interface Name {
+    name: string;
+    padding: string;
+    handleFileSelect: any
+    selected: boolean
+    selectedFile: any
+}
+
+function Tree({ children }: Children) {
+    return (
+        <div style={{ height: "auto" }}>
+            {children}
+        </div>
+    )
+}
+
+function File({ name, padding, handleFileSelect, selected, selectedFile }: Name) {
+
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+    const [mode, setMode] = React.useState<PaletteMode>(prefersDarkMode ? 'dark' : 'light');
+    const colorMode = React.useMemo(
+        () => ({
+            // The dark mode switch would invoke this method
+            toggleColorMode: () => {
+                setMode((prevMode: PaletteMode) =>
+                    prevMode === 'light' ? 'dark' : 'light',
+                );
+            },
+        }),
+        [],
+    );
+
+    // Update the theme only if the mode changes
+    const theme = React.useMemo(() => createTheme(getAllTokens(mode)), [mode]);
+
+    let ext = name.split('.')[1]
+
+    const handleClick = () => {
+        handleFileSelect(selectedFile);
+    }
+
+    if (selected) {
+        //className={'flex items-center cursor-pointer bg-zinc-600 ' + padding}
+        return (
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                backgroundColor: "zinc",
+                opacity: .7,
+                paddingLeft: padding,
+                height: "auto",
+                cursor: "pointer"
+            }} onClick={handleClick}>
+                {FILE_ICONS[ext] || <div />}
+                <span className="ml-1 font-['Consolas', 'Courier New', 'monospace'] text-sm">{name}</span>
+            </div>
+        )
+    }
+
+    //className={'flex items-center cursor-pointer hover:bg-zinc-700 ' + padding
+
+    return (
+        <div style={{
+            display: "flex",
+            alignItems: "center",
+            backgroundColor: "zinc",
+            opacity: 1,
+            paddingLeft: padding,
+            height: "auto",
+            cursor: "pointer"
+        }} onClick={handleClick}>
+            {FILE_ICONS[ext] || <div />}
+            <span className="ml-1 font-['Consolas', 'Courier New', 'monospace'] text-sm">{name}</span>
+        </div>
+    )
+}
+
+function Folder({ children, name, padding, filepath, repoId, handleFileSelect, selectedFile }: ChildrenName) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleToggle = (filepath: string, repoId: string) => {
+        GetDirectoryData(filepath, repoId)
+        setIsOpen(!isOpen);
+    }
+
+    const GetDirectoryData = async (filepath: string, repoId: string) => {
+        setLoading(true)
+        let res = await call(
+            "/api/project/getProjectDirectories",
+            "post",
+            null,
+            null,
+            null,
+            //@ts-ignore
+            { repo_id: repoId, ref: "main", filepath: filepath },
+            null,
+            config.rootPath
+        )
+        if (res !== undefined && res["message"] !== undefined) {
+            setFolderData(res["message"])
+            setLoading(false)
+        } else {
+            setLoading(false)
+            swal("Unable to get directly data at this time. Please try again later.")
+        }
+    }
+
+    const [folderData, setFolderData] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
+
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+    const [mode, setMode] = React.useState<PaletteMode>(prefersDarkMode ? 'dark' : 'light');
+    const colorMode = React.useMemo(
+        () => ({
+            // The dark mode switch would invoke this method
+            toggleColorMode: () => {
+                setMode((prevMode: PaletteMode) =>
+                    prevMode === 'light' ? 'dark' : 'light',
+                );
+            },
+        }),
+        [],
+    );
+
+    // Update the theme only if the mode changes
+    const theme = React.useMemo(() => createTheme(getAllTokens(mode)), [mode]);
+
+
+    let nameElement = name
+
+    //className={'flex items-center cursor-pointer hover:bg-zinc-700 ' + padding}
+
+    function extractNumber(name: string): number {
+        const match = name.match(/\d+/);
+        return match ? parseInt(match[0], 10) : 0
+    }
+
+    // organize the source code alphabetically first, then numerically
+    folderData.sort((a, b) => {
+        // @ts-ignore
+        const prefixA = a.name.replace(/\d+/g, '');
+        // @ts-ignore
+        const prefixB = b.name.replace(/\d+/g, '');
+
+        if (prefixA < prefixB) return -1;
+        if (prefixA > prefixB) return 1;
+
+        // @ts-ignore
+        return extractNumber(a.name) - extractNumber(b.name);
+    });
+
+
+    return (
+        <div className=''>
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                cursor: "pointer",
+                paddingLeft: padding,
+                height: "30px",
+                paddingBottom: "5px",
+            }} onClick={() =>
+                handleToggle(filepath, repoId)
+            }>
+                {isOpen ?
+                    <div>
+                        <RiArrowDownSLine />
+                        <FolderIcon style={{ fontSize: "small" }} />
+                    </div>
+                    :
+                    <div>
+                        <ButtonBase>
+                            <RiArrowRightSLine onClick={() =>
+                                GetDirectoryData(filepath, repoId)
+                            } />
+                            <FolderIcon style={{ fontSize: "small" }} />
+                        </ButtonBase>
+                    </div>
+                }
+                <span style={{ fontSize: "small", fontWeight: "normal" }}>{nameElement}</span>
+            </div>
+            <div
+                style={isOpen ? { height: "auto", overflow: "hidden" } : { height: 0, overflow: "hidden" }}>
+                {loading ? (
+                    <Box>
+                        <CircularProgress
+                            color="inherit"
+                            size={16}
+                            sx={{
+                                marginLeft: "40px"
+                            }}
+                        />
+                    </Box>
+                ) : (
+                    <div style={{
+                        paddingBottom: "5px",
+                    }}>
+                        {folderData.map((data) => {
+
+                            return (
+                                <div>
+                                    {data["type"] === "dir" ? (
+                                        // @ts-ignore
+                                        <Tree.Folder name={
+                                            //@ts-ignore
+                                            data["name"]} padding={"40px"} filepath={
+                                                //@ts-ignore
+                                                data["path"]
+                                            } repoId={repoId} handleFileSelect={handleFileSelect} selectedFile={selectedFile}>
+                                        </Tree.Folder>
+                                    ) : (
+                                        <Tree.File name={
+                                            //@ts-ignore
+                                            data["name"]} selected={selectedFile !== undefined && selectedFile === data} padding={"40px"} handleFileSelect={handleFileSelect} selectedFile={data} />
+                                    )}
+                                </div>
+                            )
+                        })}
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+Tree.File = File;
+Tree.Folder = Folder;
+
+interface FilesProps {
+    selectedFile: object
+    handleFileSelect: any
+    projectName: string
+    files: object[];
+    repoId: string
+}
+
+function Files({ selectedFile, handleFileSelect, projectName, files, repoId }: FilesProps) {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+    const [mode, setMode] = React.useState<PaletteMode>(prefersDarkMode ? 'dark' : 'light');
+    const colorMode = React.useMemo(
+        () => ({
+            // The dark mode switch would invoke this method
+            toggleColorMode: () => {
+                setMode((prevMode: PaletteMode) =>
+                    prevMode === 'light' ? 'dark' : 'light',
+                );
+            },
+        }),
+        [],
+    );
+
+    // Update the theme only if the mode changes
+    const theme = React.useMemo(() => createTheme(getAllTokens(mode)), [mode]);
+
+
+
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "fit-content",
+            height: "100%",
+            alignItems: "start",
+            paddingLeft: "5px",
+            paddingRight: "10px"
+        }}>
+            <div style={{ display: "flex", flexDirection: "row", justifyContent: "start", alignItems: "center" }}>
+                <h1 style={{ fontSize: "medium", marginLeft: "10px" }}>{"Project Files"}</h1>
+            </div>
+            <Tree>
+                {(files && files.length > 0) && files.map((file) => {
+                    return (
+                        <div style={{
+                            paddingBottom: "2.5px",
+                            fontSize: "small"
+                        }}>
+                            {
+                                //@ts-ignore
+                                file["type"] === "dir" ? (
+                                    //@ts-ignore
+                                    <Tree.Folder name={
+                                        //@ts-ignore
+                                        file["name"]} padding={"20px"} filepath={
+                                            //@ts-ignore
+                                            file["path"]
+                                        } repoId={repoId} handleFileSelect={handleFileSelect} selectedFile={selectedFile}>
+                                    </Tree.Folder>
+                                ) : (
+                                    <Tree.File name={
+                                        //@ts-ignore
+                                        file["name"]} selected={selectedFile === file} padding={"40px"} handleFileSelect={handleFileSelect} selectedFile={file} />
+                                )}
+                        </div>
+                    )
+                })}
+            </Tree>
+        </div>
+    );
+}
+
+export default Files;
---
+++ gigo-ui/src/components/editor/workspace_config/code_display_editor.tsx
@@ -0,0 +1,320 @@
+import * as React from "react";
+import { Box, Button, CircularProgress, createTheme, PaletteMode } from "@mui/material";
+import Editor, { useMonaco } from "@monaco-editor/react";
+import { useEffect, useState } from "react";
+import { DefaultWorkspaceConfig } from "../../../models/workspace";
+import darkEditorTheme from "../themes/GitHub Dark.json"
+import lightEditorTheme from "../themes/GitHub.json"
+import "../css/editor.css"
+import call from "../../../services/api-call";
+import config from "../../../config";
+import Sidebar from "../../EditorComponents/Sidebar";
+import { useNavigate } from "react-router-dom";
+import MarkdownRenderer from "../../Markdown/MarkdownRenderer";
+import { getAllTokens } from "@/theme";
+import Scrollbars from "react-custom-scrollbars";
+import { ThreeDots } from "react-loading-icons";
+import { PropaneTankSharp } from "@mui/icons-material";
+
+export interface Code_display_editor {
+    style?: object;
+    height?: number | string | undefined;
+    width?: number | string | undefined;
+    repoId: string;
+    references: string;
+    filepath: string;
+    projectName: string
+}
+
+function CodeDisplayEditor(props: Code_display_editor) {
+    let userPref = localStorage.getItem('theme')
+    const [mode, _] = React.useState<PaletteMode>(userPref === 'light' ? 'light' : 'dark');
+    const theme = React.useMemo(() => createTheme(getAllTokens(mode)), [mode]);
+    const [allFiles, setAllFiles] = React.useState([])
+    const [fileName, setFileName] = useState("")
+    const [chosenFile, setChosenFile] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    const monaco = useMonaco();
+
+    let navigate = useNavigate();
+
+    const selectFile = async (file: any) => {
+        if (!props.repoId || props.repoId === "" || !props.references || props.references === "")
+            return
+
+        if (file["content"] === undefined || file["content"] === null || file["content"] === "") {
+            let res = await call(
+                "/api/project/getProjectFiles",
+                "post",
+                null,
+                null,
+                null,
+                // @ts-ignore
+                { repo_id: props.repoId, ref: props.references, filepath: file["path"] },
+                null,
+                config.rootPath
+            )
+            file = { ...file, content: res["message"]["content"] };
+        }
+
+        setChosenFile(file)
+    }
+
+    const apiLoad = async () => {
+
+        if (!props.repoId || props.repoId === "" || !props.references || props.references === "")
+            return
+
+        setLoading(true)
+        let res = await call(
+            "/api/project/getProjectCode",
+            "post",
+            null,
+            null,
+            null,
+            // @ts-ignore
+            { repo_id: props.repoId, ref: props.references, filepath: props.filepath },
+            null,
+            config.rootPath
+        )
+
+        if (res !== undefined && res["message"] !== undefined) {
+            setAllFiles(res["message"]);
+            setFileName(res["message"][0]["name"])
+            let index = res["message"].findIndex((item: { name: string; }) => item.name === "README.md");
+
+            if (index !== -1) {
+                await selectFile(res["message"][index]);
+            } else {
+                // determine the first file that is not hidden and is not in a hidden directory
+                index = res["message"].findIndex((item: { name: string; }) => !item.name.startsWith('.'));
+                await selectFile(res["message"][index]);
+            }
+
+        }
+        setLoading(false)
+    }
+
+    const findLangauge = (name: string) => {
+        let textArray = name.split(".")
+        let determinate = textArray[1]
+        switch (determinate) {
+            // Languages with rich IntelliSense and validation
+            case 'ts':
+                return 'typescript';
+            case 'js':
+                return 'javascript';
+            case 'css':
+                return 'css';
+            case 'less':
+                return 'less';
+            case 'scss':
+                return 'scss';
+            case 'json':
+                return 'json';
+            case 'html':
+                return 'html';
+
+            // Languages with only basic syntax colorization
+            case 'xml':
+                return 'xml';
+            case 'php':
+                return 'php';
+            case 'cs':
+                return 'csharp';
+            case 'cpp':
+            case 'cxx':
+            case 'cc':
+                return 'cpp';
+            case 'cshtml':
+                return 'razor';
+            case 'md':
+            case 'markdown':
+                return 'markdown';
+            case 'diff':
+                return 'diff';
+            case 'java':
+                return 'java';
+            case 'vb':
+                return 'vb';
+            case 'coffee':
+                return 'coffeescript';
+            case 'hbs':
+                return 'handlebars';
+            case 'bat':
+                return 'batch';
+            case 'pug':
+                return 'pug';
+            case 'fs':
+            case 'fsharp':
+                return 'fsharp';
+            case 'lua':
+                return 'lua';
+            case 'ps1':
+                return 'powershell';
+            case 'py':
+                return 'python';
+            case 'rb':
+                return 'ruby';
+            case 'sass':
+                return 'sass';
+            case 'r':
+                return 'r';
+            case 'm':
+                return 'objective-c';
+            case 'go':
+                return 'go';
+            case 'sql':
+                return 'sql';
+            case 'swift':
+                return 'swift';
+            case 'sh':
+                return 'shell';
+            case 'yml':
+            case 'yaml':
+                return 'yaml';
+            case 'rs':
+                return 'rust';
+            case 'clj':
+                return 'clojure';
+            case 'kt':
+                return 'kotlin';
+            case 'pl':
+                return 'perl';
+            case 'hs':
+                return 'haskell';
+            case 'erl':
+                return 'erlang';
+            case 'elixir':
+                return 'elixir';
+            case 'elm':
+                return 'elm';
+            case 'dart':
+                return 'dart';
+            default:
+                return 'plaintext';
+        }
+    }
+
+    useEffect(() => {
+        if (monaco) {
+            monaco.editor.defineTheme("gigo-default-light", lightEditorTheme)
+            monaco.editor.defineTheme("gigo-default-dark", darkEditorTheme)
+            monaco.editor.setTheme(mode === "light" ? "gigo-default-light" : "gigo-default-dark")
+        }
+    }, [monaco, mode]);
+
+    useEffect(() => { 
+        apiLoad()
+    }, [props.references, props.repoId]);
+
+    const handleCurrentFileSideBar = (file: any) => {
+        setLoading(true)
+        selectFile(file)
+        setLoading(false)
+    }
+
+    //@ts-ignore
+    const isMarkdown = chosenFile["name"]
+        //@ts-ignore
+        ? findLangauge(chosenFile["name"]).toLowerCase() === "markdown"
+        : false;
+
+    if (!props.repoId || props.repoId === "" || !props.references || props.references === "")
+        return null
+
+    return (
+        <div style={props.style}>
+            <div style={{ width: "50%", display: "flex", height: "73vh" }}>
+                <Sidebar
+                    selectedFile={chosenFile}
+                    handleFileSelect={handleCurrentFileSideBar}
+                    projectNames={props.projectName}
+                    files={allFiles}
+                    repoId={props.repoId}
+                />
+            </div>
+            {
+                isMarkdown ? (
+                    <Scrollbars
+                        style={{
+                            height: props.height,
+                            width: "121vw",
+                            border: `1px solid ${theme.palette.primary.contrastText}`,
+                            borderRadius: 5,
+                        }}
+                        renderThumbVertical={({ style, ...props }) =>
+                            <div {...props}
+                                style={{
+                                    ...style,
+                                    backgroundColor: theme.palette.primary.contrastText,
+                                    borderRadius: 10
+                                }}
+                            />
+                        }
+                    >
+                        {/*@ts-ignore*/}
+                        <MarkdownRenderer markdown={chosenFile["content"]} style={{
+                            height: props.height,
+                            width: "60vw",
+                            overflowWrap: "break-word",
+                            padding: "2em 3em",
+                        }} />
+                    </Scrollbars>
+                )
+                    : (
+                        loading ? (
+                            <Box
+                                sx={{
+                                    border: `1px solid ${theme.palette.primary.contrastText}`,
+                                    boxSizing: 'border-box',
+                                    width: "121vw",
+                                    height: props.height,
+                                    borderRadius: "5px",
+                                }}
+                            >
+                                <CircularProgress
+                                    color="inherit"
+                                    sx={{
+                                        // center the spinner in the middle of the Box
+                                        position: 'absolute',
+                                        top: '52vh',
+                                        left: '59vw',
+                                        transform: 'translate(-50%, -50%)'
+                                    }}
+                                />
+                            </Box>
+                        ) : (
+                            <Editor
+                                options={{ readOnly: true }}
+                                height={props.height}
+                                width={"121vw"}
+                                path={
+                                    //@ts-ignore
+                                    chosenFile["name"]}
+                                value={
+                                    //@ts-ignore
+                                    chosenFile["content"]}
+                                //@ts-ignore
+                                language={chosenFile["name"] !== undefined ? findLangauge(chosenFile["name"]).toLowerCase() : ""}
+                                theme={mode === "light" ? "gigo-default-light" : "gigo-default-dark"}
+                                className={"yaml-editor"}
+                                wrapperProps={{
+                                    style: {
+                                        border: `1px solid ${theme.palette.primary.contrastText}`,
+                                        boxSizing: 'border-box',
+                                        width: "121vw",
+                                        height: props.height,
+                                        borderRadius: 5,
+                                    }
+                                }}
+                            />
+                        )
+                    )
+            }
+        </div>
+    )
+}
+
+export default CodeDisplayEditor;
